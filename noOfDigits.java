ef06287afdd5207a8157a7b780e0d62242a1a4ba
Somnath Singharoy <106828063+somu545@users.noreply.github.com>
2023-10-14 15:30:38 +0530

    Add files via upload

---
+++ noOfDigits.java
@@ -0,0 +1,10 @@
+public class noOfDigits {
+    public static void main(String[] args) {
+        int num = 32;
+        int base = 2;
+
+        int ans = (int)(Math.log(num) / Math.log(base)) +1 ;
+        System.out.println(ans);
+
+    }
+}
